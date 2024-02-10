c1745d4e9c14f1b1f60e4f43464a54cbe32c2be5
alifer597 <156974184+alifer597@users.noreply.github.com>
2024-02-10 13:18:05 -0300

    Add files via upload

---
+++ quizdd.py
@@ -0,0 +1,281 @@
+from os import *
+from time import *
+
+redes = ["micael", "fernando", "fernandão", "fernandao", "estefano", "estéfano", "barba", "siquini", "cebola", "rodrigo", "marcelo", "mica"]
+redes_b = ["drex","diogenes", "diógenes","vitão", "victor", "arantes", "b2","didi"]
+noc = ["alifer", "boca rica", "gabe", "fido dido", "rich mouth", "agiota","bilolo","melkiss","nathazinho","duzao","duzão", "gustavo", "gabriel", "eduardo","barbara","cavaleiro da morte", "cavaleiro da guerra", "cavaleiro da peste", "mobral", "bárbara", "marcos", "marcão", "melks", "b1","miles", "guerreiro", "nalber", "guilherme","lucas", "nathan", "chinelo", "rafael", "bololo", "bruno", "esteban", "pedro","pedrão", "pedrao", "para-raio da desgraça", "bochecha", "giovani", "leandro","david", "bazinha"]
+alta_c = ["fábio", "fabio", "diniz", "pigari", "saulo", "avelino", "ave", "cafelinux","bahia"]
+itms = ["cleber", "clebao", "cléber", "mazza", "putífero", "putifero", "pedrinho", "everton", "roberta", "scaranti"]
+finados = ["cristiano","leandra", "thalles", "tadala", "paulinho", "richard", "jean", "danilo", "cavaleiro da fome"]
+cima = ["leticia", "letícia", "ceifadora", "bel"]
+outros = ["geberson","nagamine"]
+
+def suspense():
+    system('clear')
+    sleep(1)
+    print(".")
+    sleep(1)
+    system('clear')
+    print("..")
+    sleep(1)
+    system('clear')
+    print("...")
+    sleep(1)
+    system('clear')
+
+def intro_effect():
+    sleep(4)
+    system('clear')
+
+
+def intro():
+
+  sleep(2)    
+  print("Esse é o Quiz da Discórdia...")
+  intro_effect()
+  print("Usaremos palavrões e botaremos o dedo na ferida!")
+  intro_effect()
+  print("Se isso for demais pra você...")
+  intro_effect()
+  print("SAIA DAQUI ENQUANTO É TEMPO!")
+  intro_effect()
+  print("Não diga que não avisei!")
+  intro_effect()
+  print("Teimoso do CARALHO!")
+  intro_effect()
+  print("QUE OS JOGOS COMECEM APÓS A CONTAGEM REGRESSIVA!")
+  intro_effect()
+  print("10")
+  sleep(1)
+  system('clear')
+  print("9")
+  sleep(1)
+  system('clear')
+  print("8")
+  sleep(1)
+  system('clear')
+  print("7")
+  sleep(1)
+  system('clear')
+  print("6")
+  sleep(1)
+  system('clear')
+  print("5")
+  sleep(1)
+  system('clear')
+  print("4")
+  sleep(1)
+  system('clear')
+  print("3")
+  sleep(1)
+  system('clear')
+  print("2")
+  sleep(1)
+  system('clear')
+  print("1")
+  sleep(1)
+  system('clear')
+
+intro()
+
+
+
+
+
+
+
+
+
+def discordia():
+  print('''Esse é o Quiz da Discórdia\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⣠⣴⡾⠟⠋⠉⠉⠉⠉⠉⠉⠛⠿⣦⣀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣷⡀⠀⠀⠀
+  ⠀⠀⢠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⠀⠀
+  ⠀⢠⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⠀
+  ⢀⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⣤⡄⠘⣿⡀
+  ⣸⡇⠀⢿⡿⠿⠿⢿⡿⠿⠿⣿⡇⠀⣿⡋⠉⠹⠏⠉⣉⣿⠏⠀⢹⡇
+  ⣿⠀⠀⠈⢿⣦⣀⣈⣡⣤⡾⠟⠁⠀⠘⢿⣶⣶⣶⠿⠛⠁⠀⠀⢸⡇
+  ⢻⡄⠀⠀⠀⢉⡙⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+  ⢸⡇⠀⠀⠀⠸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣦⠀⠀⢀⣿⠃
+  ⠀⢿⡄⠀⠀⠀⠹⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣿⠃⠀⢠⣾⠏⠀
+  ⠀⠈⢷⡄⠀⠀⠀⠙⢿⣦⣄⣀⣀⣀⣠⣤⣶⡿⠛⠁⢀⣴⡿⠃⠀⠀
+  ⠀⠀⠈⠻⣦⣀⠀⠀⠀⠈⠉⠛⠛⠛⠋⠉⢀⣀⣤⣶⠿⠋⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠈⠙⠛⠷⠶⢶⣶⣶⡶⠶⠾⠛⠛⠛⠉⠀⠀⠀⠀⠀ ''')
+  while True:
+   discordia1 = input("\nEscolha o nível de maldade: 'Alto' ou 'Alto'\n").lower()
+
+   if discordia1 == "alto" or discordia1 == "a":
+    suspense()
+    print("\nOk, vamos começar com alto!\n")
+    break
+   else:
+    print("\nEscolha inválida.\nRevise a resposta e não faça gracinhas (ARROMBADO)\n")
+
+  while True:
+    pg1 = input("Se você pudesse salvar um Analista de Redes do fuzilamento ou comprar uma Ferrari.\nQual cor de Ferrari você escolheria?\n1- Preta\n2- Vermelha\n3- Prata\n").lower()
+    suspense()
+    if pg1 == "vermelha" or pg1 == "2":
+       print("\nBoa, já vi que você curte o clássico.\nVermelho igual o alerta do SITE que você não pingou!\n")
+       break
+    elif pg1 == "preta" or pg1 == "1":
+       print("\nNunca vi uma Ferrari preta.\nMas vi que tem bom gosto. A cor da Ferrari vai combinar com o fundo da sala ZOOM em que eles vão se foder, HU HU HU\n")
+       break
+    elif pg1 == "prata" or pg1 == "3":
+       print("\nCor sem graça pra por numa Ferrari, parece uma piada do Cristiano.\nMas gosto é gosto!\n")
+       break
+    else:
+       print("\nVocê é daltônico ou analfabeto ?!\nAqui não tem figura pra ler, Guerreiro!\n")
+
+
+
+
+  while True:
+    pg2 = input("\nSe a Ceifadora te chama pra conversar com um papel sulfite na mão. O que você faz?\n1- Grita 'FODEU'\n2- Comemoro\n3- Não tenho medo de Ceifadoras\n").lower()
+    suspense()
+    if pg2 == "1" or pg2 == "fodeu":
+        print("\nAcho que é hora de mijar no gerador e ir embora!\nSó não vale murchar os pneus do celtinha =O\n")
+        break
+    elif pg2 == "2" or pg2 == "comemoro":
+        print("\nQuando é assim vai gastar a recisão no Maiytrea ou fazer carinho naquele colega de trabalho com uma barra de ferro!\n")
+        break
+    elif pg2 == "3" or pg2 == "não tenho medo de ceifadoras":
+        print("\nEntão segue seu caminho, aproveita e lança o telefone do Marlon no Bate Papo da UOL\n")
+        break
+    else:
+        print("Ta me estressando, porra! Vou fumar um cigarro!")
+
+  while True:
+    pg3 = input("\nMinha intuição ta boa hoje. Consigo acertar sua personalidade dependendo de qual finado você escolher:\n1- Cprates\n2- Tbezerra\n3- Lmoura\n4- Tadala\n5- PaulinPaulada\n").lower()
+    suspense()
+    if pg3 == "1" or pg3 == "cprates":
+        print("\nVocê gosta de novos desafios, mesmo que não esteja nenhum pouco preparado. Mas quando o problema surge, você sai da sala pra atender ligação falsa.\nSua personalidade é: IMPULSIVO e COVARDE\n")
+        break
+    elif pg3 == "2" or pg3 == "tbezerra":
+        print("\nVocê curte aproveitar a vida, maços de cigarro e noitadas insanas. Sinto ameaças de massacre na sua aura.\nSua personalidade é: AVENTUREIRO e PSICOPATA\n")
+        break
+    elif pg3 == "3" or pg3 == "lmoura":
+        print("\nVocê gosta de espairecer e viajar, mesmo que pra isso tenha que faltar no trabalho.\nSua personalidade é: FOLGADO e PRÓDIGO\n")
+        break
+    elif pg3 == "4" or pg3 == "tadala":
+        print("\nVocê constantemente entra em atrito com todos a sua volta por ser intrometido.\nSua personalidade é: BOYZINHO e FILHO DA PUTA\n")
+        break
+    elif pg3 == "5" or pg3 == "paulinpaulada":
+        print("\nVocê faz tudo no seu tempo, até cochila em treinamento. Não gosta muito de contato.\nSua personalidade é: PANGUÃO e INDIVIDUALISTA\n")
+        break
+    else:
+        print("\nResponde direto!\nEsses erros estão mais chatos que cobrança de bolo!\n")
+
+  while True:
+    pg4 = input("\nVocê saiu de almoço/janta e o Bolívia é seu parceiro no 12x36. Se vocês sobreviverem o final de semana, seu salário vai pra 5k. Mas ele terá que enfrentar um caso perigoso. Qual desses você escolheria?\n1- Cervejaria Nova TIO isolada com Lindomar de plantonista\n2- Queda das principais sessões BGP daquele cliente com os procedimentos escrotos\n3- Login das antenas da Smurf Telecom\n").lower()
+    suspense()
+    if pg4 == "1" or pg4 == "cervejaria":
+       print("\nCaralho, você merece o aumento só pela coragem de escolher essa opção. Mas seu aumento foi de tempo livre, os dois vão pra rua nessa!\n")
+       break
+    elif pg4 == "2" or pg4 == "escroto":
+       print("\nOs caras reclamam até quando escalona certo. Mais certo que isso só seu dinheiro indo embora!\n")
+       break    
+    elif pg4 == "3" or pg4 == "smurf":
+       print("Pipocou na resposta, seu ganancioso. Sabe que é capaz dele bloquear o usuário no login e não conseguir do mesmo jeito.\n")
+       break
+    else:
+        print(f"Éééé, mas você diz: {pg4}\nÉ a gente que tem que fazer? Hijo de puta, la concha de tu madre!\n")
+
+
+  while True:
+
+    pg6 = input("Digamos que você ficou sem bonificação, o crescimento anual não foi o esperado:\n1- Acontece\n2- To puto\n3- To puto with lasers\n").lower()
+    suspense()
+    if pg6 == "1" or pg6 == "acontece":
+        print("No mínimo você já ta com bolso cheio de dinheiro pra ta nessa paz!\nDinheirinho de pinga pra você!\n")
+        break
+    elif pg6 == "2" or pg6 == "to puto":
+        print("Ta puto? Você ta puto e pobre, isso sim!\nVamo rir de nervoso kkkk\n")
+        break
+    elif pg6 == "3" or pg6 == "to puto with lasers":
+        print("Ei! Nada de juntar todo mundo pra pegar o que time que 'Traz Novos Clientes' no soco!\nControlem-se(Opcional)\n")
+        break
+    else:
+        print("Sem bônus e sem ter o que fazer pelo jeito!\nPresta atenção no que escreve, Mobral\n")
+
+
+
+  while True:
+    pg5 = input("\nChegamos ao final desse QUIZ. Você gostou dessa porra?\n1- Sim\n2- Talvez\n3- Não\n").lower()
+    suspense()
+    if pg5 == "1" or pg5 == "sim":
+       print("\nÉ, até que deu pro gasto. Já ligou na unidade pra validar a energia hoje?\n")
+       break
+    elif pg5 == "2" or pg5 == "talvez":
+       print("\nEm cima do muro? Então vai dar uma geral na sua fila. E essa 'Causa da Indisponibilidade' vazia?\n")
+       break    
+    elif pg5 == "3" or pg5 == "não" or pg5 == "nao":
+       print("\nBota uma dentadura no cu e ri pro caralho, mermão. RIP Gil Brother\n")
+       break
+    else:
+        print("\nCansei, escreve o que você quiser! FIM!\n")
+
+
+
+
+def filtragem():
+  while True:
+
+    jogador = input("Digite como te chamam dentro da PP. Nome, apelido ou sobrenome.\nSe alguém do NOC tiver o mesmo nome que você, use sobrenome ou apelido: (FODA-SE que você foi contratado primeiro!)\n").lower()
+    if jogador in redes_b:
+        suspense()
+        print("Você eu vou deixar passar, mesmo sendo de Redes!\n")
+        discordia()
+        break
+    elif jogador in redes:
+        suspense()
+        print("Putz!\nVocê é de redes, foi mal!\nACESSO NEGADO!\n")
+        break
+    elif jogador in noc:
+        suspense()
+        print("Passe Livre aos membros do Purgatório!\nAcesso Vitalício!\n")
+        discordia()
+        break
+    elif jogador in alta_c:
+        suspense()
+        print("Se você tá aqui, as coisas tomaram proporções maiores do que imaginei!\nAcesso Concedido!\n")
+        discordia()
+        break
+    elif jogador in itms:
+        suspense()
+        print("Até um dos sistemáticos por aqui!\nAcesso Concedido!\n")
+        discordia()
+        break
+    elif jogador in finados:
+        suspense()
+        print("Deixem os mortos em paz!\nAcesso Negado!\n")
+        break
+    elif jogador in cima:
+        suspense()
+        print("Sei de nada!\nAcesso Concedido!\n")
+        discordia()
+        break
+    elif jogador in outros:
+        suspense()
+        print("Só vai passar por ser boa praça\nAcesso Concedido!\n")
+        discordia()
+        break
+    else:
+        suspense()
+        print("Você escreveu o próprio nome errado ou não tem relevância para este jogo!\nAcesso Negado!\n")
+
+
+filtragem()
+
+while True:
+    rpg = input("\nQuer jogar de novo? 'S' ou 'N'?\n").lower()
+    suspense()
+    if rpg == "sim" or rpg == "s":
+        print("\nOcioso do Caralho\n") 
+        intro()
+        filtragem()
+    elif rpg == "nao" or rpg == "n" or rpg == "não":
+        print("\nVai se foder, então!\n")
+        break
+    else: 
+        print("\nLouco a gente tem que deixar falando sozinho!")
+        break
